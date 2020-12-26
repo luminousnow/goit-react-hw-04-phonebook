4c5700f1fa8d4f430ab0c852d89852c202a0d422
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-26 02:25:26 +0200

    add coments

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,8 +6,7 @@ function ContactForm({ getContactData }) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
-  // ===
-  // динамічно рендерить зміну полів інпутів
+  // === rerender input fields value === //
   const handleImputChange = e => {
     const { name, value } = e.currentTarget;
 
@@ -26,17 +25,17 @@ function ContactForm({ getContactData }) {
     }
   };
 
-  // виконується при Сабміті форми
+  // on press submit button
   const onSubmitPress = e => {
     e.preventDefault();
 
-    // результати полів прокидає в State Арр.js
+    // send values to State Арр.js
     getContactData(name, number);
 
     resetFormField();
   };
 
-  // очищає поля форми після Submit
+  // clean up form fields after form submission
   const resetFormField = () => {
     setName('');
     setNumber('');
@@ -80,70 +79,3 @@ function ContactForm({ getContactData }) {
 }
 
 export default ContactForm;
-
-// class oldContactForm extends Component {
-//   // state = {
-//   //   name: '',
-//   //   number: '',
-//   // };
-
-//   // // динамічно рендерить зміну полів інпутів
-//   // handleImputChange = e => {
-//   //   const { name, value } = e.currentTarget;
-//   //   this.setState({ [name]: value });
-//   // };
-
-//   // // виконується при Сабміті форми
-//   // onSubmitPress = e => {
-//   //   e.preventDefault();
-
-//   //   // результати полів прокидає в State Арр.js
-//   //   this.props.getContactData(this.state);
-
-//   //   this.resetFormField();
-//   // };
-
-//   // // очищає поля форми після Submit
-//   // resetFormField = () => {
-//   //   this.setState({ name: '', number: '' });
-//   // };
-
-//   render() {
-//     const { name, number } = this.state;
-//     const { onSubmitPress, handleImputChange } = this;
-
-//     return (
-//       <form onSubmit={onSubmitPress} className={s.form}>
-//         <fieldset className={s.fieldset}>
-//           <label className={s.label}>
-//             <span>name</span>
-//             <input
-//               type="text"
-//               name="name"
-//               value={name}
-//               onChange={handleImputChange}
-//               className={s.input}
-//             />
-//           </label>
-//           <label className={s.label}>
-//             <span>number</span>
-//             <input
-//               type="tel"
-//               name="number"
-//               value={number}
-//               onChange={handleImputChange}
-//               className={s.input}
-//             />
-//           </label>
-//           <button
-//             type="submit"
-//             className={s.button}
-//             disabled={!(name && number)}
-//           >
-//             <span>add contacts</span>
-//           </button>
-//         </fieldset>
-//       </form>
-//     );
-//   }
-// }
